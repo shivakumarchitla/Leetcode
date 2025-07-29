275c4a40cf6f2f7c3a0f0d4954f72878f393e0f6
Shiva kumar <shivakumarchitla09@gmail.com>
2025-07-29 10:20:17 +0530

    Time: 3 ms (88.55%) | Memory: 17.8 MB (78.92%) - LeetSync

---
+++ 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        a = 0
+        
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] % (nums2[j]*k) == 0:
+                    a+=1
+        return a
+
+        
