c345b13c78e93864d80cb1ddee463c650857710b
Shiva kumar <shivakumarchitla09@gmail.com>
2025-08-18 10:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (20.92%) - LeetSync

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        if n %2 == 1:
+            return "a" * n
+        else:
+            return "a" * (n - 1) + "b"
+
+
+            
+        
