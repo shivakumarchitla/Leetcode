315123a39fbc56d650c791d39f0c32c368ca8e98
Shiva kumar <shivakumarchitla09@gmail.com>
2025-09-02 21:45:04 +0530

    Time: 4 ms (85.49%) | Memory: 19.8 MB (9.08%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        n = []
+        l = []
+        q = []
+        for i in nums:
+            if i%2==0:
+                n.append(i)
+            else:
+                l.append(i)
+        for j in range(len(n)):
+            q.append(n[j])
+            q.append(l[j])
+        return q
+            
+        
