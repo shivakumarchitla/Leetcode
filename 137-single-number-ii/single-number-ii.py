8e9b28adcf5b6840c694fd646d6f445c2305158b
Shiva kumar <shivakumarchitla09@gmail.com>
2025-09-02 23:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (32.61%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n = {}
+        for i in nums:
+            if i in n:
+                n[i]+=1
+            else:
+                n[i] = 1
+        for j in n:
+            if n[j]==1:
+                return j
+        
