f4e9fbe34a477e0963b9e96f7a32851ba93f92f5
Shiva kumar <shivakumarchitla09@gmail.com>
2025-10-08 11:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (26.63%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,14 +1,10 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         nums.append(target)
-        k = []
-        for i in range(len(nums)):
-            if nums[i] not in k:
-                k.append(nums[i])
-        m = sorted(k)
-        for j in range(len(m)):
-            if m[j]==target:
-                return j
         
+        k = sorted(nums)
+        for i in range(len(k)):
+            if k[i]==target:
+                return i
 
         
